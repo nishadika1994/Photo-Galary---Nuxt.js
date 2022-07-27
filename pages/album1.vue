d143caa3806f7d4565ae8c140133621b1d519190
NipuniWickramasinghe <81399550+NipuniWickramasinghe@users.noreply.github.com>
2022-07-28 01:31:37 +0530

    Photo Galary View

---
+++ pages/album1.vue
@@ -0,0 +1,91 @@
+<template>
+<div class="main">
+  <div class="heading">
+    <h2>ALBUM {{alubumNum}}</h2>
+  </div>
+    <div class="albums" v-for="item in images" :key="item.id">
+          <div v-if="item.albumId == alubumNum" class="img-container">
+            <a :href="item.url" target="_blank"><img :src="item.url"></a>
+            <h1>Album ID : {{ item.albumId }} Image ID : {{ item.id }}</h1>
+          </div>
+    </div>
+</div>   
+</template>
+<script>
+export default {
+    data(){
+        return{
+            images: null,
+            alubumNum : null
+        }
+    },
+    created() {
+            this.images = this.$route.params.userId
+            this.alubumNum = this.$route.params.albumID
+            console.log(this.$route.params.userId)
+        },
+    }
+</script>
+<style scoped>
+h2{
+  position: relative;
+  font: 3em sans-serif;
+  font-size: 400%;
+
+}
+h1{
+  position: relative;
+  font: 3em sans-serif;
+  font-size: 150%;
+  padding: 20px;
+  font: 20px Arial, sans-serif;
+
+}
+.img-container {
+ width:25%;
+ float:left;
+}
+img{
+  max-width:100%;
+}
+.main{
+  margin: 100px;
+}
+.heading{
+  text-align: center;
+}
+.link {
+  display: flex;
+  align-items: center;
+
+  cursor: pointer;
+  position: relative;
+  font-weight: 600;
+  user-select: none;
+  font: bold;
+  font-size: 300%;
+
+  margin: 0.1em 0;
+  padding: 0.4em;
+  border-radius: 0.25em;
+  height: 1.5em;
+
+  color:rgb(85, 85, 87);
+  text-decoration: none;
+}
+
+.link:hover {
+  background-color: var(--sidebar-item-hover);
+}
+
+.link.active {
+  background-color: var(--sidebar-item-active);
+}
+::v-deep .el-button{
+  width: 300px;
+  height: 300px;
+  margin: 20px;
+  font: bold;
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="main">
+  <div class="heading">
+    <H1>Photo Galary</H1>
+  </div>
+    <div class="albums">
+      <el-button class="btn1"><nuxt-link :to="{ name: 'album1', params: { userId: posts , albumID: 1 }}" >Alubum 01</nuxt-link></el-button>
+      <el-button class="btn2"><nuxt-link :to="{ name: 'album1', params: { userId: posts , albumID: 2 }}" >Alubum 02</nuxt-link></el-button>
+      <el-button class="btn3"><nuxt-link :to="{ name: 'album1', params: { userId: posts , albumID: 3 }}" >Alubum 03</nuxt-link></el-button><br>
+      <el-button class="btn4"><nuxt-link :to="{ name: 'album1', params: { userId: posts , albumID: 4 }}" >Alubum 04</nuxt-link></el-button>
+      <el-button class="btn5"><nuxt-link :to="{ name: 'album1', params: { userId: posts , albumID: 5 }}" >Alubum 05</nuxt-link></el-button>
+      <el-button class="btn6"><nuxt-link :to="{ name: 'album1', params: { userId: posts , albumID: 6 }}" >Alubum 06</nuxt-link></el-button>
+  </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+var images;
+export default {
+    name: 'consume-rest-api',
+    data(){
+        return{
+            posts: null
+        }
+    },
+    created() {
+      debugger
+        axios.get(`https://jsonplaceholder.typicode.com/photos`) 
+            .then(response => {
+                this.posts = response.data    
+                images = response['data'] 
+                console.log(images)   
+                debugger 
+            })
+            .catch(e => {
+                console.log("Error Please check the connection") 
+            });
+    },
+   
+}
+
+</script>
+<style scoped>
+h1{
+  position: relative;
+  font: 3em sans-serif;
+  font-size: 400%;
+
+}
+.main{
+  margin: 100px;
+}
+.albums{
+  margin-left: 200px;
+  
+}
+.heading{
+  text-align: center;
+}
+.link {
+  display: flex;
+  align-items: center;
+
+  cursor: pointer;
+  position: relative;
+  font-weight: 600;
+  user-select: none;
+  font: bold;
+  font-size: 300%;
+
+  margin: 0.1em 0;
+  padding: 0.4em;
+  border-radius: 0.25em;
+  height: 1.5em;
+
+  color:rgb(85, 85, 87);
+  text-decoration: none;
+}
+
+.link:hover {
+  background-color: var(--sidebar-item-hover);
+}
+
+.link.active {
+  background-color: var(--sidebar-item-active);
+}
+::v-deep .el-button{
+  width: 300px;
+  height: 300px;
+  margin: 20px;
+  font: bold;
+}
+.btn1{
+    background-color: blue;
+    font: bold;
+    font-size: 200%;
+}
+.btn2{
+    background-color: orangered;
+        font: bold;
+    font-size: 200%;
+}
+.btn3{
+    background-color: yellow;
+    font: bold;
+    font-size: 200%;
+}
+.btn4{
+    background-color: green;
+    font: bold;
+    font-size: 200%;
+}
+.btn5{
+    background-color: darkblue;
+    font: bold;
+    font-size: 200%;
+}
+.btn6{
+    background-color: red;
+    font: bold;
+    font-size: 200%;
+}
+</style>
